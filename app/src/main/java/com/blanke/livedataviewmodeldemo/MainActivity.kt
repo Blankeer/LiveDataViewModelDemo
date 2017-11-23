c27c5cf6063e29769db3f13748a38b9cb49aeb88
Blanke <blankeeee@gmail.com>
2017-11-23 11:24:40 +0800

    [ADD] 去掉 LiveData, 尝试只使用 ObservableField ,原因是 LiveData 不能自动绑定到 xml 里,必须 observe 用 java 代码设置

--- app/src/main/java/com/blanke/livedataviewmodeldemo/MainActivity.kt
+++ app/src/main/java/com/blanke/livedataviewmodeldemo/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.blanke.livedataviewmodeldemo
 
-import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.os.Bundle
@@ -14,14 +13,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         mainBing = DataBindingUtil.setContentView(this, R.layout.activity_main)
         mainBing.viewModal = ViewModelProviders.of(this).get(NewsViewModel::class.java)
-        with(mainBing.viewModal) {
-            this!!.news.observe(this@MainActivity, Observer<News> {
-                if (it == null) return@Observer
-                with(mainBing) {
-                    title.text = it.title
-                    content.text = it.content
-                }
-            })
-        }
     }
 }
--- app/src/main/java/com/blanke/livedataviewmodeldemo/NewsViewModel.kt
+++ app/src/main/java/com/blanke/livedataviewmodeldemo/NewsViewModel.kt
@@ -2,24 +2,24 @@ package com.blanke.livedataviewmodeldemo
 
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
-import android.arch.lifecycle.MutableLiveData
 import android.databinding.ObservableBoolean
+import android.databinding.ObservableField
 
 /**
  * Created by blanke on 2017/11/22.
  */
 
 class NewsViewModel(application: Application) : AndroidViewModel(application) {
-    val news: MutableLiveData<News> = MutableLiveData()
     private val newsRepo: NewsRepo = NewsRepo()
     val loading = ObservableBoolean(false)
+    val news = ObservableField<News>()
 
     fun loadData() {
         loading.set(true)
         newsRepo.loadData(object : NewsRepo.Call {
             override fun onLoadData(news: News) {
                 loading.set(false)
-                this@NewsViewModel.news.value = news
+                this@NewsViewModel.news.set(news)
             }
         })
     }
